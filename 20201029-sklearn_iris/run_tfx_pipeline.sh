72aa97c33b3901a98b7fe58a453a05b360cc8a80
Patrick McCarthy <pmccarthy@dstillery.com>
2020-10-29 17:30:49 +0000

    new run with tfx source at tag/v0.24.1, tfx==0.24.1

--- 20201029-sklearn_iris/run_tfx_pipeline.sh
+++ 20201029-sklearn_iris/run_tfx_pipeline.sh
@@ -14,7 +14,7 @@ conda activate tfx_024_iris_sklearn
 cp -r tfx-source/tfx/examples/iris $HOME/
 
 # This seems like a dependency changed
-sed -i "s/tfx.dsl.component/tfx.component/" $HOME/iris/experimental/iris_pipeline_sklearn_local.py
+# sed -i "s/tfx.dsl.component/tfx.component/" $HOME/iris/experimental/iris_pipeline_sklearn_local.py
 
 python $HOME/iris/experimental/iris_pipeline_sklearn_local.py
 
